19c47ffa648427a7c7430a7efe3c4df917c5df32
Bandan Chakrabortty <110911912+HiBandan@users.noreply.github.com>
2022-08-19 16:43:30 +0200

    Add files via upload

---
+++ modelTissueFlow/README.md
@@ -0,0 +1,4 @@
+# How to use tissue flow
+## How to install module
+
+<p> How to get data .... <p> 
---
+++ modelTissueFlow/__init__.py
@@ -0,0 +1,3 @@
+from .modules import  inOutTools,analysisModule
+from .modelTissueFlow import Animal1D,System1D,tissueFlow1D
+
