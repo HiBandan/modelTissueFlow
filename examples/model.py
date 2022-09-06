34367dbded0d787fafe8d64c89eb1a161d5d552d
Bandan Chakrabortty <110911912+HiBandan@users.noreply.github.com>
2022-09-07 01:16:51 +0200

    Add files via upload

---
+++ examples/model.py
@@ -0,0 +1,15 @@
+import time
+import analysis_movies
+from modelTissueFlow import Genotype1D
+
+# start-time
+start_time = time.time()
+# initialize-tissue-flow
+tF = Genotype1D()
+# modelling
+tF.run_MODEL(outputPath=analysis_movies.movie_path,parametersFileName='./modelling_parameters.txt',figFormat='pdf')
+# end-time    
+print("--- %s minutes ---" % ((time.time() - start_time)/60))    
+    
+
+
