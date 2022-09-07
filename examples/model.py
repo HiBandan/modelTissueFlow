6f8c8e0c33e6078d4f145cb17bff20730006aee7
Bandan Chakrabortty <110911912+HiBandan@users.noreply.github.com>
2022-09-07 07:35:21 +0200

    Update model.py

--- examples/model.py
+++ examples/model.py
@@ -5,9 +5,9 @@ from modelTissueFlow import Genotype1D
 # start-time
 start_time = time.time()
 # initialize-tissue-flow
-tF = Genotype1D()
+GENOTYPE = Genotype1D()
 # modelling
-tF.run_MODEL(outputPath=analysis_movies.movie_path,parametersFileName='./modelling_parameters.txt',figFormat='png')
+GENOTYPE.run_MODEL(outputPath=analysis_movies.movie_path,parametersFileName='./modelling_parameters.txt',figFormat='png')
 # end-time    
 print("--- %s minutes ---" % ((time.time() - start_time)/60))    
     
