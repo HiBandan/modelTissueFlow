7a06432f0c085fe8946a0b1091f94cf144004d6d
Bandan Chakrabortty <110911912+HiBandan@users.noreply.github.com>
2022-09-07 07:23:49 +0200

    Update plot_analysis.py

--- examples/plot_analysis.py
+++ examples/plot_analysis.py
@@ -9,10 +9,10 @@ for gen_counter,gen_ID in enumerate(analysis_movies.GenoTypes.keys()):
     # load-genotype-data
     GENOTYPE = inOutTools.load_data_via_pickle(inPutPath=analysis_movies.data_path+'/output_'+gen_ID) 
     # view-genotype(embryo-averaged)-data
-    GENOTYPE.view_piv(figFormat='pdf')
+    GENOTYPE.view_piv(figFormat='png')
     # loop-over-EMBRYOS
     for embryo_counter,EMBRYO in enumerate(GENOTYPE.ANIMALS):
         # view-individual-embryo-data
-        EMBRYO.view_frames(parametersFileName='./plot_parameters.txt',figFormat='pdf')
+        EMBRYO.view_frames(parametersFileName='./plot_parameters.txt',figFormat='png')
 # end-time    
 print("--- %s minutes ---" % ((time.time() - start_time)/60))
