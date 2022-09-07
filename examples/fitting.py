85393b6c3f36d70bd8fe19ad6d17ed3bee6bcd58
Bandan Chakrabortty <110911912+HiBandan@users.noreply.github.com>
2022-09-07 07:24:26 +0200

    Update fitting.py

--- examples/fitting.py
+++ examples/fitting.py
@@ -9,7 +9,7 @@ tF = Genotype1D()
 # loop-over-GENOTYPES
 for gen_counter,gen_ID in enumerate(analysis_movies.GenoTypes.keys()):   
     # fitting      
-    tF.run_FITTING(expDataPath=analysis_movies.data_path+'/output_'+ gen_ID,parametersFileName='./fit_parameters.txt',hypothesis='curvature',changingParameter='lh',parameters_FIXED={},BOUNDARY_COND_SWITCH = 'PERIODIC',spatial_fitting_domain=[],fit_piv_Type = 'piv',figFormat='pdf')
+    tF.run_FITTING(expDataPath=analysis_movies.data_path+'/output_'+ gen_ID,parametersFileName='./fit_parameters.txt',hypothesis='curvature',changingParameter='lh',parameters_FIXED={},BOUNDARY_COND_SWITCH = 'PERIODIC',spatial_fitting_domain=[],fit_piv_Type = 'piv',figFormat='png')
 # end-time    
 print("--- %s minutes ---" % ((time.time() - start_time)/60))    
     
